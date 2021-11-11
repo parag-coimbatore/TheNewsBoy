6fc8dee6793687a30a2a28be62a0c51e5cdc8201
Parag Coimbatore <paragcoimbatore745@gmail.com>
2021-11-11 18:02:10 +0530

    done with changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,8 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary">
           <div className="container-fluid">
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/0/07/News.png"
@@ -13,17 +14,20 @@ export class Navbar extends Component {
               width="30"
               height="24"
             />
-            
-            <Link className="navbar-brand" to="/" my-300>
+
+            <Link className="navbar-brand" to="/" >
               TheNewsBoy
             </Link>
-        
+
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
             <div
               className="collapse navbar-collapse"
               id="navbarSupportedContent"
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                
+
                 <li>
                   <Link className="nav-link" to="/Business">
                     Business
@@ -59,16 +63,11 @@ export class Navbar extends Component {
                     Technology
                   </Link>
                 </li>
-                <li>
-                  <Link className="nav-link" to="/About">
-                    About
-                  </Link>
-                </li>
+                
               </ul>
             </div>
           </div>
         </nav>
-        
       </div>
     );
   }
